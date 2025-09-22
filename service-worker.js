2a7e9363bf0d094949d079424ae58755c2629dbd
YASHU141104 <127117031@sastra.ac.in>
2025-09-22 14:23:06 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+
+// Service worker for offline support (PWA)
+// Caches core files for offline access
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('lni-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/', 
+        '/index.html', 
+        '/style.css', 
+        '/app.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => 
+      response || fetch(event.request)
+    )
+  );
+});
